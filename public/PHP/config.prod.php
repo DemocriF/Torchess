05fde240342c0ea6cd5ea4e6db44c506bd41427c
DemocriF <fardin.democri@mail.mcgill.ca>
2025-03-01 20:49:34 -0500

    Correction of config connection to db

--- public/PHP/config.prod.php
+++ public/PHP/config.prod.php
@@ -6,7 +6,7 @@ $db_password = "Fardin1380";
 $dbname = "torchess_db";    // production database name (can be different if needed)
 
 // Create connection
-$conn = new mysqli($servername, $db_username, $db_password, $dbname);
+$conn = new mysqli("127.0.0.1", "db_user", "db_password", "torchess_db", 3306);
 
 // Check connection
 if ($conn->connect_error) {
