7e0c899f9c2c6b219fa082e03b2500b439ec87db
DemocriF <fardin.democri@mail.mcgill.ca>
2025-03-01 22:06:54 -0500

    Working on signin sign up pages

--- public/PHP/signup.php
+++ public/PHP/signup.php
@@ -60,7 +60,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   if ($stmt->execute()) {
     // Registration successful; log the user in
     $_SESSION['user_id'] = $stmt->insert_id;
-    header("Location: PHP/account.php");
+    header("Location: ../HTML/MainPage.html");
     exit();
   } else {
     header("Location: signup.html?error=" . urlencode("Signup failed. Please try again."));
